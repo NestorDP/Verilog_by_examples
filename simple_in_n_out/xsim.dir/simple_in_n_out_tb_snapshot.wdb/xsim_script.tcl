746a2fc7695a57cd77ba1972fe0a6b2e566ca8ed
NestorDP <nestor-dp@hotmail.com>
2024-03-12 22:52:25 -0300

    deleted auxiliar files and apdate the .gitignore

--- simple_in_n_out/xsim.dir/simple_in_n_out_tb_snapshot.wdb/xsim_script.tcl
+++
@@ -1,4 +0,0 @@
-set_param project.enableReportConfiguration 0
-load_feature core
-current_fileset
-xsim {simple_in_n_out_tb_snapshot.wdb} -autoloadwcfg
--- simple_in_n_out/xsim.dir/simple_in_n_out_tb_snapshot/xsim_script.tcl
+++
@@ -1,1 +0,0 @@
-xsim {simple_in_n_out_tb_snapshot} -autoloadwcfg -tclbatch {sim/xsim_cfg.tcl}
